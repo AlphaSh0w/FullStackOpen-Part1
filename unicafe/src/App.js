ac945d930c93323eed873cd3dfcce43b76945dd9
Alphashow <lastblast@outlook.fr>
2021-05-31 19:01:41 +0100

    step1 unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({onClick,text}) => {
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
 }
 
-export default App;
+const FeedBackButtons = ({goodOnClick,neutralOnClick,badOnClick}) => {
+  return(
+    <>
+      <p>Give feedback</p>
+      <Button onClick={goodOnClick} text="good"/>
+      <Button onClick={neutralOnClick} text="neutral"/>
+      <Button onClick={badOnClick} text="bad"/>
+    </>
+  )
+} 
+
+const Statistics = ({good,neutral,bad}) => {
+  return(
+    <p>
+      good {good}<br></br>
+      neutral {neutral}<br></br>
+      bad {bad}<br></br>
+    </p>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+      <FeedBackButtons goodOnClick={() => setGood(good+1)}
+       neutralOnClick={() => setNeutral(neutral+1)}
+       badOnClick={() => setBad(bad+1)}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
